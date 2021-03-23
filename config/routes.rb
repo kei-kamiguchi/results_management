d6f4d5068a0c9bfce75fb288e6b5c70f301b2a8c
keikamiguchi <kei.kamiguchi517@gmail.com>
2021-03-23 19:33:53 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'time_cards#index'
+  devise_for :users
+  root 'time_cards#new'
   resources :time_cards
   resources :work_results
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
